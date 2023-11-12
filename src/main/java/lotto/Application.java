b8a1e289e5d5d95819211f77f9bee898a605c690
parang <69078515+pjy2163@users.noreply.github.com>
2023-11-12 18:08:34 +0900

    refactor : 불필요한 import 삭제, controller만 배치

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,9 @@
 package lotto;
 
-import lotto.controller.LottoMainController;
-import lotto.domain.*;
+import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoManager lottoManager = new LottoManager(
-                new CreateLotto(),
-                new WinReward(),
-                new ProfitRate());
-        LottoMainController lottoController = new LottoMainController(lottoManager);
+        LottoController lottoController = new LottoController();
         lottoController.run();
     }
-}
