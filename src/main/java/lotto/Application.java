8be5cc07dd013c0523d0ff0da31ae14fa3379305
parang <69078515+pjy2163@users.noreply.github.com>
2023-11-12 17:22:24 +0900

    refactor : import domain * 으로 통합

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,7 @@
 package lotto;
 
 import lotto.controller.LottoMainController;
-import lotto.domain.CreateLotto;
-import lotto.domain.ProfitRate;
-import lotto.domain.WinReward;
+import lotto.domain.*;
 
 public class Application {
     public static void main(String[] args) {
